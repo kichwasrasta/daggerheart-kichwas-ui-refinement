282ed756e9ab151b91bb0a77aa35d7d83ca1322f
kichwasrasta <kichwas@yahoo.com>
2026-04-22 16:30:07 -0700

    Turn the spellcast trait on the header breadcrumb into a rollable action - so when a GM calls for spellcast roll players will just click it and then not worry about finding the right attritubte.

--- scripts/inject-spellcasting.js
+++ scripts/inject-spellcasting.js
@@ -9,12 +9,11 @@ Hooks.on("renderCharacterSheet", (app, html, data) => {
   if (data.document.system.spellcastModifierTrait?.key) {
     const spellcastHtml = `
       <span class="dot">•</span>
-      <span class="spellcasting-trait">
-        <i title="${game.i18n.localize('DAGGERHEART.ITEMS.Subclass.spellcastingTrait')}" class="fa-solid fa-wand-sparkles"></i>
+      <span class="spellcasting-trait" title="${game.i18n.localize('DAGGERHEART.ITEMS.Subclass.spellcastingTrait')}: ${data.document.system.spellcastModifierTrait.key}" data-action="rollAttribute" data-attribute="${data.document.system.spellcastModifierTrait.key}" data-value="${data.document.system.spellcastModifierTrait.value}">
+        <i class="fa-solid fa-wand-sparkles"></i>
         ${game.i18n.localize(`DAGGERHEART.CONFIG.Traits.${data.document.system.spellcastModifierTrait.key}.short`)}
       </span>
     `;
-
     const characterDetails = html.querySelector(".character-details");
     if (characterDetails) {
       const allSpans = characterDetails.querySelectorAll("span");
